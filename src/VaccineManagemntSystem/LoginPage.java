6b77449ca004f9aec7839136366b796047848779
dineshdhayfule <dineshdhayfule@gmail.com>
2022-08-01 14:07:13 +0530

    First Commit

---
+++ src/VaccineManagemntSystem/LoginPage.java
@@ -0,0 +1,107 @@
+package VaccineManagemntSystem;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+//import com.jgoodies.forms.layout.FormLayout;
+//import com.jgoodies.forms.layout.ColumnSpec;
+//import com.jgoodies.forms.layout.RowSpec;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Canvas;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.JButton;
+import java.awt.CardLayout;
+import java.awt.Font;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class LoginPage extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginPage frame = new LoginPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 350);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(230, 230, 250));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 500, 350);
+		panel.setBackground(new Color(127, 255, 212));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("LOGIN");
+		lblNewLabel.setFont(new Font("Tempus Sans ITC", Font.BOLD, 29));
+		lblNewLabel.setBounds(183, 23, 117, 87);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("USER NAME");
+		lblNewLabel_1.setFont(new Font("Tempus Sans ITC", Font.BOLD, 20));
+		lblNewLabel_1.setBounds(34, 120, 117, 30);
+		panel.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(182, 120, 178, 30);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("PASSWORD");
+		lblNewLabel_1_1.setFont(new Font("Tempus Sans ITC", Font.BOLD, 20));
+		lblNewLabel_1_1.setBounds(34, 179, 117, 30);
+		panel.add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(183, 179, 178, 30);
+		panel.add(textField_1);
+		
+		JButton btnNewButton = new JButton("LOGIN");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				setVisible(false);
+			}
+		});
+		btnNewButton.setBorderPainted(false);
+		btnNewButton.setBackground(new Color(0, 0, 0));
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 29));
+		btnNewButton.setBounds(179, 245, 148, 55);
+		panel.add(btnNewButton);
+		setUndecorated(true);
+
+	}
+}
